716afd0f043abe19cd06153922f0bcc06871870e
MikaelInnowise <mikael.isayan@innowise.com>
2025-10-30 18:45:53 +0300

    Change next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
   assetPrefix: "./",
   images: {
@@ -10,7 +9,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  output: 'standalone',
+  output: "standalone",
 };
 
 export default nextConfig;
