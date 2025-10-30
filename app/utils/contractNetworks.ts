b30fdfd6b45e0c17782249e384def11cb6c0afdd
Mikael Isayan <mikael.isayan@innowise.com>
2025-10-30 17:58:37 +0300

    Update contractNetworks.ts with new addresses
    Branch 1.4.1-3

--- app/utils/contractNetworks.ts
+++ app/utils/contractNetworks.ts
@@ -30,17 +30,21 @@ const localContractNetworks = {
     // Add any other required addresses here
   },
   // Haust Mainnet (Chain ID: 3864)
-  3864: {
-    safeSingletonAddress: "0x8dBD3A49Fa6f53BBd082aa99d91c8ADeFbA8E7E7",
-    safeProxyFactoryAddress: "0x38E8Acc939923dEac6162Ac174b9683bbE4Dc945",
-    multiSendAddress: "0x7B21BBDBdE8D01Df591fdc2dc0bE9956Dde1e16C",
-    multiSendCallOnlyAddress: "0x32228dDEA8b9A2bd7f2d71A958fF241D79ca5eEC",
-    fallbackHandlerAddress: "0xcB4a8d3609A7CCa2D9c063a742f75c899BF2f7b5",
-    signMessageLibAddress: "0x309C7b0A0D2f250Be322739753386911E1187C4E",
-    createCallAddress: "0x8BbCaE989A0Bdf15c8E783357a0E5848e36233d0",
-    simulateTxAccessorAddress: "0xB59bD9861a97F9c309B7b73338503507580625D2",
-    tokenCallbackHandlerAddress: "0x63117fd9761850f4aC685457E484A01D752D5cC4",
-  },
+ 3864: {
+        "safeSingletonAddress": "0x639245e8476E03e789a244f279b5843b9633b2E7",
+        "safeProxyFactoryAddress": "0xd9d2Ba03a7754250FDD71333F444636471CACBC4",
+        "multiSendAddress": "0x7B21BBDBdE8D01Df591fdc2dc0bE9956Dde1e16C",
+        "multiSendCallOnlyAddress": "0x32228dDEA8b9A2bd7f2d71A958fF241D79ca5eEC",
+        "fallbackHandlerAddress": "0xcB4a8d3609A7CCa2D9c063a742f75c899BF2f7b5",
+        "signMessageLibAddress": "0x309C7b0A0D2f250Be322739753386911E1187C4E",
+        "createCallAddress": "0x8BbCaE989A0Bdf15c8E783357a0E5848e36233d0",
+        "simulateTxAccessorAddress": "0xB59bD9861a97F9c309B7b73338503507580625D2",
+        "tokenCallbackHandlerAddress": "0x63117fd9761850f4aC685457E484A01D752D5cC4",
+        "safeToL2SetupAddress": "0x5DC954024c42433BF567eb072992A83b4d480A0a",
+        "safeL2Address": "0x76667330c237Fb40f28d74563cdAAae4b06C23Ec",
+        "safeToL2MigrationAddress": "0xf91b2B39B185395502Da5DD797bB659A169ba2E4",
+        "safeMigrationAddress": "0x2E16757bA45834aE4f106d24A525a6002166E1F9"
+    },
 };
 
 // Helper to build contractNetworks for selected chainIds and Safe version
